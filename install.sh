8fcace5bd6a276738db2df680e1d136792126569
dingzifan719 <ding719@foxmail.com>
2023-07-23 15:36:54 +0800

    feat:update

--- install.sh
+++ install.sh
@@ -6,3 +6,12 @@ call mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.
 call mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.equinox.preferences -Dversion=3.7.500.v20190815-1535 -Dpackaging=jar -Dfile=./jars/org.eclipse.equinox.preferences_3.7.500.v20190815-1535.jar -DgeneratePom=true
 call mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.equinox.registry -Dversion=3.8.500.v20190714-1850  -Dpackaging=jar -Dfile=./jars/org.eclipse.equinox.registry_3.8.500.v20190714-1850.jar -DgeneratePom=true
 call mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.osgi -Dversion=3.15.0.v20190830-1434 -Dpackaging=jar -Dfile=./jars/org.eclipse.osgi_3.15.0.v20190830-1434.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.antlr -DartifactId=antlr4-runtime -Dversion=4.6 -Dpackaging=jar -Dfile=./jars/antlr4-runtime-4.6.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=com.fasterxml.jackson.core -DartifactId=jackson-databind -Dversion=2.9.10.1 -Dpackaging=jar -Dfile=./jars/jackson-databind-2.9.10.1.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=com.alibaba -DartifactId=fastjson -Dversion=1.2.58 -Dpackaging=jar -Dfile=./jars/fastjson-1.2.58.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.json -DartifactId=json -Dversion=20210307 -Dpackaging=jar -Dfile=./jars/json-20210307.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=com.google.code.gson -DartifactId=gson -Dversion=2.8.8 -Dpackaging=jar -Dfile=./jars/gson-2.8.8.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=info.picocli -DartifactId=picocli -Dversion=4.6.2 -Dpackaging=jar -Dfile=./jars/picocli-4.6.2.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.eclipse.core.filesystem.linux -DartifactId=x86 -Dversion=1.1.0-v20070416 -Dpackaging=jar -Dfile=./jars/x86-1.1.0-v20070416.jar	-DgeneratePom=true
+call mvn install:install-file -DgroupId=com.fasterxml.jackson.core -DartifactId= jackson-annotations -Dversion=2.9.10.1 -Dpackaging=jar -Dfile=./jars/jackson-annotations-2.9.10.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=com.fasterxml.jackson.core -DartifactId= jackson-core -Dversion=2.9.10.1 -Dpackaging=jar -Dfile=./jars/jackson-core-2.9.10.jar -DgeneratePom=true
